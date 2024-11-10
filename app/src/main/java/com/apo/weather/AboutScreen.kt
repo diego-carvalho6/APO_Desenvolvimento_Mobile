dfa8ea738659460b6a9034d17501af558a87d9cb
diego <diego.oliveira@bornlogic.com>
2024-11-10 12:53:59 -0300

    imports

--- app/src/main/java/com/apo/weather/AboutScreen.kt
+++ app/src/main/java/com/apo/weather/AboutScreen.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.apo.weather.ui.theme.WeatherTheme
 
